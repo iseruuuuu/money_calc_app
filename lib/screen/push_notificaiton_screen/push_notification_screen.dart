4affbbcfaf53a6fa2a1f32de96a35a4d1cb3d974
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-08 07:34:20 +0900

    fix: 通知画面をstatelessにしていた

--- lib/screen/push_notificaiton_screen/push_notification_screen.dart
+++ lib/screen/push_notificaiton_screen/push_notification_screen.dart
@@ -1,117 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:money_calc_app/model/color.dart';
 import 'package:money_calc_app/model/notification/date_service.dart';
-import 'package:money_calc_app/model/notification/push_notification.dart';
 import 'package:money_calc_app/preference/preference.dart';
-import 'package:top_snackbar_flutter/custom_snack_bar.dart';
-import 'package:top_snackbar_flutter/top_snack_bar.dart';
+import 'package:get/get.dart';
+import 'package:money_calc_app/screen/push_notificaiton_screen/push_notification_screen_controller.dart';
 
-class PushNotificationScreen extends StatefulWidget {
+class PushNotificationScreen extends StatelessWidget {
   const PushNotificationScreen({
     Key? key,
-    required this.currentMonth,
   }) : super(key: key);
-
-  final int currentMonth;
-
-  @override
-  _PushNotificationScreenState createState() => _PushNotificationScreenState();
-}
-
-class _PushNotificationScreenState extends State<PushNotificationScreen> {
-  int monthToPresent = -1;
-  String month = "";
-  int _amountOfDaysToPresent = 0;
-  int selectedDays = 0;
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    getPreference();
-    _amountOfDaysToPresent = DateService().amountOfDaysInMonth(
-        DateService().convertMonthToWord(widget.currentMonth));
-    monthToPresent = widget.currentMonth;
-    month = DateService().convertMonthToWord(monthToPresent);
-    NotificationService().init(_onDidReceiveLocalNotification).whenComplete(
-          () => NotificationService()
-              .handleApplicationWasLaunchedFromNotification(""),
-        );
-  }
-
-  getPreference() async {
-    final pref = await Preference().getInt(PreferenceKey.days);
-    setState(() {
-      selectedDays = pref;
-    });
-  }
-
-  @override
-  void didUpdateWidget(PushNotificationScreen oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    _amountOfDaysToPresent = DateService().amountOfDaysInMonth(
-      DateService().convertMonthToWord(widget.currentMonth),
-    );
-  }
-
-  Future<dynamic> _onDidReceiveLocalNotification(
-      int id, String? title, String? body, String? payload) async {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => AlertDialog(
-        title: const Text('給料は受け取れましたか？'),
-        content: const Text('忘れないように登録をおすすめします！'),
-        actions: [
-          TextButton(
-            child: const Text("Ok"),
-            onPressed: () {
-              NotificationService()
-                  .handleApplicationWasLaunchedFromNotification(payload ?? '');
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> scheduleNotification() async {
-    final now = DateTime.now();
-    final days = await Preference().getInt(PreferenceKey.days);
-    for (int i = 0; i < 12; i++) {
-      int _id = i;
-      //DateTime _date = DateTime(now.year, now.month, now.day, 16, 51 + i, 20);
-      DateTime _date = DateTime(now.year, now.month + i, days, 10, 00, 00);
-      const detail = NotificationDetails(
-        android: AndroidNotificationDetails('id', 'name'),
-        iOS: IOSNotificationDetails(),
-      );
-      await flutterLocalNotificationsPlugin.schedule(
-        _id,
-        '給料は受け取れましたか？',
-        '忘れないように登録をおすすめします！',
-        _date,
-        detail,
-      );
-    }
-  }
-
-  void cancelAll() async {
-    await flutterLocalNotificationsPlugin.cancelAll();
-  }
-
-  void popUpDialog() {
-    showTopSnackBar(
-      context,
-      CustomSnackBar.success(
-        message: "毎月$selectedDays日に通知が来ます！",
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(PushNotificationScreenController(), tag: '');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColor.red2,
@@ -130,12 +31,16 @@ class _PushNotificationScreenState extends State<PushNotificationScreen> {
             flex: 1,
             child: Padding(
               padding: const EdgeInsets.only(top: 50),
-              child: Text(
-                //登録した日付を登録する。
-                (selectedDays != 0) ? '通知：毎月$selectedDays日' : '未設定',
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+              child: Obx(
+                () => Text(
+                  //登録した日付を登録する。
+                  (controller.selectedDays != 0)
+                      ? '通知：毎月${controller.selectedDays}日'
+                      : '未設定',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
@@ -151,32 +56,16 @@ class _PushNotificationScreenState extends State<PushNotificationScreen> {
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
                             crossAxisCount: 5),
-                    itemCount: _amountOfDaysToPresent,
+                    itemCount: controller.amountOfDaysToPresent,
                     shrinkWrap: true,
                     itemBuilder: (BuildContext context, int index) {
                       final date =
                           DateService().constructDateTimeFromDayAndMonth(
                         day: (index + 1),
-                        month: widget.currentMonth,
+                        month: controller.currentMonth,
                       );
                       return TextButton(
-                        onPressed: () {
-                          setState(() {
-                            selectedDays = date.day;
-                          });
-
-                          //選択した日付
-                          Preference().setInt(PreferenceKey.days, date.day);
-
-                          //すでに登録されていたら、通知を全て消す。→上書きできていることが確認できた。
-                          cancelAll();
-
-                          //通知を出現する
-                          scheduleNotification();
-
-                          //pop upみたいに登録できたことを連絡する。
-                          popUpDialog();
-                        },
+                        onPressed: () => controller.onTap(date),
                         child: FittedBox(
                           fit: BoxFit.fitWidth,
                           child: Column(
---
+++ lib/screen/push_notificaiton_screen/push_notification_screen_controller.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:money_calc_app/model/notification/date_service.dart';
+import 'package:money_calc_app/model/notification/push_notification.dart';
+import 'package:money_calc_app/preference/preference.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
+
+class PushNotificationScreenController extends GetxController {
+  int monthToPresent = -1;
+  String month = "";
+  int amountOfDaysToPresent = 0;
+  int selectedDays = 0;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  final int currentMonth = DateService().getCurrentMonthNumber();
+
+  @override
+  void onInit() {
+    super.onInit();
+    getPreference();
+    getMonth();
+  }
+
+  void getMonth() {
+    amountOfDaysToPresent = DateService()
+        .amountOfDaysInMonth(DateService().convertMonthToWord(currentMonth));
+    monthToPresent = currentMonth;
+    month = DateService().convertMonthToWord(monthToPresent);
+    NotificationService().init(_onDidReceiveLocalNotification).whenComplete(
+          () => NotificationService()
+              .handleApplicationWasLaunchedFromNotification(""),
+        );
+  }
+
+  void onTap(DateTime date) {
+      selectedDays = date.day;
+
+    //選択した日付
+    Preference().setInt(PreferenceKey.days, date.day);
+
+    //すでに登録されていたら、通知を全て消す。→上書きできていることが確認できた。
+    cancelAll();
+
+    //通知を出現する
+    scheduleNotification();
+
+    //TODO pop upみたいに登録できたことを連絡する。
+    //popUpDialog();
+  }
+
+  getPreference() async {
+    final pref = await Preference().getInt(PreferenceKey.days);
+    selectedDays = pref;
+  }
+
+  // @override
+  // void didUpdateWidget(PushNotificationScreen oldWidget) {
+  //   super.didUpdateWidget(oldWidget);
+  //   _amountOfDaysToPresent = DateService().amountOfDaysInMonth(
+  //     DateService().convertMonthToWord(widget.currentMonth),
+  //   );
+  // }
+
+  Future<dynamic> _onDidReceiveLocalNotification(
+      int id, String? title, String? body, String? payload) async {
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('給料は受け取れましたか？'),
+        content: const Text('忘れないように登録をおすすめします！'),
+        actions: [
+          TextButton(
+            child: const Text("Ok"),
+            onPressed: () {
+              NotificationService()
+                  .handleApplicationWasLaunchedFromNotification(payload ?? '');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> scheduleNotification() async {
+    final now = DateTime.now();
+    final days = await Preference().getInt(PreferenceKey.days);
+    for (int i = 0; i < 12; i++) {
+      int _id = i;
+      //DateTime _date = DateTime(now.year, now.month, now.day, 16, 51 + i, 20);
+      DateTime _date = DateTime(now.year, now.month + i, days, 10, 00, 00);
+      const detail = NotificationDetails(
+        android: AndroidNotificationDetails('id', 'name'),
+        iOS: IOSNotificationDetails(),
+      );
+      await flutterLocalNotificationsPlugin.schedule(
+        _id,
+        '給料は受け取れましたか？',
+        '忘れないように登録をおすすめします！',
+        _date,
+        detail,
+      );
+    }
+  }
+
+  void cancelAll() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+
+  void popUpDialog() {
+  }
+  //   showTopSnackBar(
+  //     Get.context!,
+  //     CustomSnackBar.success(
+  //       message: "毎月$selectedDays日に通知が来ます！",
+  //     ),
+  //   );
+  // }
+}
