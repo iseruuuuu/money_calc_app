f7ba6a1ed9eb4c6241dbd54adc3062a105c5e019
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-20 08:10:05 +0900

    add: appbarの枠の色を統一した

--- lib/component/appbar/app_bar_item.dart
+++ lib/component/appbar/app_bar_item.dart
@@ -15,6 +15,11 @@ class AppBarItem extends StatelessWidget {
     return PreferredSize(
       preferredSize: const Size.fromHeight(0.0),
       child: AppBar(
+        shape: Border(
+          bottom: BorderSide(
+            color: color,
+          ),
+        ),
         //backgroundColor: AppColor.red2,
         backgroundColor: color,
         elevation: 0,
