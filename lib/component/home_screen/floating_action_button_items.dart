9c54b80cf2b0348c2f9b24369d2238dbc2e28275
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-20 08:12:09 +0900

    fix: change icon size

--- lib/component/home_screen/floating_action_button_items.dart
+++ lib/component/home_screen/floating_action_button_items.dart
@@ -16,9 +16,9 @@ class FloatingActionButtonItems extends StatelessWidget {
       child: FloatingActionButton(
         backgroundColor: const Color(0xFFEA6762),
         onPressed: onTap,
-        child: const Icon(
+        child: Icon(
           Icons.add,
-          size: 40,
+          size: MediaQuery.of(context).size.width / 9,
         ),
       ),
     );
