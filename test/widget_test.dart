7090bb9c89a4c4e9c4a9aa19d97c393c1fe76744
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-20 08:16:17 +0900

    fix: remove import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:money_calc_app/model/todo.dart';
 
-import 'package:money_calc_app/screen/main.dart';
 
 void main() {
   group('listのテスト', () {
