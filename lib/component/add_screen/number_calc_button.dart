c46606294cf7000bf183a28f7da8074c439f93da
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-21 14:03:05 +0900

    fix: 使わない引数を削除した

--- lib/component/add_screen/number_calc_button.dart
+++ lib/component/add_screen/number_calc_button.dart
@@ -1,37 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
-import '../../model/color.dart';
+import 'package:money_calc_app/model/color.dart';
 
 class NumberCalcButton extends StatelessWidget {
   final String text;
-  final int fillColor;
-  final int textColor;
+  final Color fillColor;
+  final Color textColor;
   final double textSize;
-  final Function callback;
+  final Function() onTap;
 
   const NumberCalcButton({
     Key? key,
     required this.text,
     required this.fillColor,
-    this.textColor = 0xFFFFFFFF,
+    required this.textColor,
     this.textSize = 28,
-    required this.callback,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 2, bottom: 2, right: 0, left: 0),
+      margin: const EdgeInsets.only(top: 0, bottom: 0, right: 0, left: 0),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(70),
-        color: AppColor.grey3,
+        border: Border.all(color: AppColor.grey2, width: 0.2),
+        borderRadius: BorderRadius.circular(200),
+        color: fillColor,
       ),
       child: SizedBox(
-        width: MediaQuery.of(context).size.width / 4.5,
-        height: MediaQuery.of(context).size.width / 4.5,
+        width: MediaQuery.of(context).size.height / 8,
+        height: MediaQuery.of(context).size.height / 8,
         child: Theme(
-          data: ThemeData(splashColor: Colors.white),
+          data: ThemeData(
+            splashColor: Colors.white,
+          ),
           child: Material(
             elevation: 0,
             clipBehavior: Clip.hardEdge,
@@ -41,14 +42,11 @@ class NumberCalcButton extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(50),
                 ),
-                onPressed: () {
-                  callback(text);
-                  HapticFeedback.selectionClick();
-                },
+                onPressed: onTap,
                 child: Text(
                   text,
                   style: TextStyle(
-                    color: Colors.black,
+                    color: textColor,
                     fontSize: textSize,
                     fontWeight: FontWeight.bold,
                   ),
