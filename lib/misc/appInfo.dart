7495435bbc62ddcc7d4fb5a0d32400953c819954
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-22 13:51:25 +0900

    remove: remove app info

---
+++ lib/misc/appInfo.dart
@@ -0,0 +1,17 @@
+// // TODO Implement this library.
+//
+// import 'package:package_info_plus/package_info_plus.dart';
+//
+// late String appName;
+// late String packageName;
+// late String version;
+// late String buildNumber;
+//
+// Future<void> init() async {
+//   PackageInfo packageInfo = await PackageInfo.fromPlatform();
+//
+//   appName = packageInfo.appName;
+//   packageName = packageInfo.packageName;
+//   version = packageInfo.version;
+//   buildNumber = packageInfo.buildNumber;
+// }
