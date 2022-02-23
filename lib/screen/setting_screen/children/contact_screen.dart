d15be1d18a9f2ed03bcfd95d26cc9e8c55987f44
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-23 16:58:56 +0900

    add: build android

--- lib/screen/setting_screen/children/contact_screen.dart
+++ lib/screen/setting_screen/children/contact_screen.dart
@@ -1,28 +1,40 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:money_calc_app/constants/app_color.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
-class ContactScreen extends StatelessWidget {
-  const ContactScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.red2,
-        title: const Text(
-          'お問い合わせ',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
-          ),
-        ),
-      ),
-      body: const WebView(
-        initialUrl: 'https://forms.gle/dcJyKYPFPN8wNTdN7',
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
-    );
-  }
-}
+// import 'dart:io';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:money_calc_app/constants/app_color.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+//
+// class ContactScreen extends StatefulWidget {
+//   const ContactScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   ContactScreenState createState() => ContactScreenState();
+// }
+//
+// class ContactScreenState extends State<ContactScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (Platform.isAndroid) WebView.platform = AndroidWebView();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: AppColor.red2,
+//         title: const Text(
+//           'お問い合わせ',
+//           style: TextStyle(
+//             fontWeight: FontWeight.bold,
+//             fontSize: 25,
+//           ),
+//         ),
+//       ),
+//       body: WebView(
+//         initialUrl: 'https://forms.gle/dcJyKYPFPN8wNTdN7',
+//         javascriptMode: JavascriptMode.unrestricted,
+//       ),
+//     );
+//   }
+// }
