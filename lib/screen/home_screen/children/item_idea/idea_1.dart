f8e4f1c1e2a4401f9f1d7fa67684b25ff2b1b1bd
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-02-24 14:50:59 +0900

    add: セルのデザインを作ってみた

--- lib/screen/home_screen/children/item_idea/idea_1.dart
+++ lib/screen/home_screen/children/item_idea/idea_1.dart
@@ -6,48 +6,68 @@ class Idea1 extends StatelessWidget {
     Key? key,
     required this.title,
     required this.day,
+    required this.isSex,
   }) : super(key: key);
 
   final String title;
   final String day;
+  final bool isSex;
 
   @override
   Widget build(BuildContext context) {
     final deviceWidth = MediaQuery.of(context).size.width;
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 30),
-      height: 90,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(13),
-        boxShadow: const [
-          BoxShadow(
-            offset: Offset(0, 17),
-            blurRadius: 23,
-            spreadRadius: -13,
-            color: Colors.red,
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          //画像を入れる
-          Icon(Icons.add),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-                Text(day),
-              ],
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: deviceWidth / 20, vertical: deviceWidth / 70),
+      child: Container(
+        //height: 90,
+        height: deviceWidth / 5,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(13),
+          boxShadow: const [
+            BoxShadow(
+              offset: Offset(0, 17),
+              blurRadius: 23,
+              spreadRadius: -13,
+              color: Colors.black87,
             ),
-          ),
-        ],
+          ],
+        ),
+        child: Row(
+          children: [
+            SizedBox(width: deviceWidth / 20),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    day,
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: deviceWidth / 25,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    title + '円',
+                    style: TextStyle(
+                      fontSize: deviceWidth / 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //Image.asset('assets/images/1.png'),
+            isSex
+                ? Image.asset('assets/images/5.png')
+                : Image.asset('assets/images/4.png'),
+            SizedBox(width: deviceWidth / 30),
+          ],
+        ),
       ),
     );
   }
